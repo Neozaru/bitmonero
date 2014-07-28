695b726f2cf4a5520452ba0f288992f759159c91
Neozaru <neozaru@mailoo.org>
2014-07-28 08:17:53 +0200

    Added 'block_height' and 'spent_block_height' in 'incoming_transfers' RPC cmd

--- src/wallet/wallet_rpc_server_commans_defs.h
+++ src/wallet/wallet_rpc_server_commans_defs.h
@@ -148,12 +148,16 @@ namespace wallet_rpc
     bool spent;
     uint64_t global_index;
     std::string tx_hash;
+    uint64_t block_height;
+    uint64_t spent_block_height;
 
     BEGIN_KV_SERIALIZE_MAP()
       KV_SERIALIZE(amount)
       KV_SERIALIZE(spent)
       KV_SERIALIZE(global_index)
       KV_SERIALIZE(tx_hash)
+      KV_SERIALIZE(block_height)
+      KV_SERIALIZE(spent_block_height)
     END_KV_SERIALIZE_MAP()
   };
 
